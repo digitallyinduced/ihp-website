5be79f726d8a219eba528491248200ccc8e3d428
Marc Scholten <marc@digitallyinduced.com>
2021-03-17 17:22:13 +0100

    Added event zoom link

--- Web/View/Layout.hs
+++ Web/View/Layout.hs
@@ -38,8 +38,8 @@ layoutWithMeta bodyClass metaTags inner = H.docTypeHtml ! A.lang "en" $ [hsx|
     </script>
 </head>
 <body class={classes ["d-flex flex-column min-vh-100", (bodyClass, True)]}>
-    <a class="d-block bg-primary event-header py-2 text-center text-white" href="https://ihp-community-events.mailchimpsites.com/" target="_blank">
-        Join our first Global IHP Meetup, 17.03.2021 18:00 CET 🚀
+    <a class="d-block bg-primary event-header py-2 text-center text-white" href="https://us02web.zoom.us/j/81343017580" target="_blank">
+        Join our first Global IHP Meetup, live in a few minutes 🚀
     </a>
     <nav class="navbar navbar-expand-lg navbar-light">
         <a class="navbar-brand" href="/"><img src="/ihp.svg" class="d-inline-block align-top" style="height: 2rem"/></a>
