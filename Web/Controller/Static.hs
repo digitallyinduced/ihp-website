d0ab0f8da4a045da89bc966658abdb7c876cb6f9
Marc Scholten <marc@digitallyinduced.com>
2021-03-17 20:08:27 +0100

    Updated slack link

--- Web/Controller/Static.hs
+++ Web/Controller/Static.hs
@@ -19,4 +19,4 @@ instance Controller StaticController where
         -- This slack link expires every 30 days. To avoid updating 20 links everywhere we have this
         -- central endpoint at http://ihp.digitallyinduced.com/Slack that just redirects to the current
         -- active slack invite url.
-        redirectToUrl "https://join.slack.com/t/ihpframework/shared_invite/zt-mfek5t9g-ipfT1BrcJolOWB3ueDrqlA"
+        redirectToUrl "https://join.slack.com/t/ihpframework/shared_invite/zt-nvf0lyte-PBUBH8_U_dndhfFr~s7Kxg"
